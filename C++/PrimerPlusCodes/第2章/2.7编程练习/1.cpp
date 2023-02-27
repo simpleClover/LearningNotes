abe5cb31d3f1914bf7506c6ce936429c4d74bed3
simpleClover <3138947771@qq.com>
2023-02-28 00:32:46 +0800

    Update

---
+++ C++/PrimerPlusCodes/第2章/2.7编程练习/1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Name: 小明" << endl;
+    cout << "Address: China" << endl;
+    return 0;
+}
---
+++ C++/PrimerPlusCodes/第2章/2.7编程练习/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l;
+    cout << "请输入一个以long为单位的距离：";
+    cin >> l;
+    cout << "这个距离等于" << l*220 << "码" << endl;
+    return 0;
+}
